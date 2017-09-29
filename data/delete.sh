0a990c86cd14436b6a867ede2a0b032cb4047233
tetsuyas <hoge@aaa.cccc>
2017-09-29 20:57:14 +0900

    解凍したファイルをutf8に変換して保存

---
+++ data/delete.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm dl/postal_code/KEN_ALL.csv
+rm dl/postal_code/ken_all.zip
+rm dl/postal_code/KEN_ALL.csv.utf8
+
+rm dl/senkyoku/CCAminToSenkyokuKokai.zip
+rm -rf dl/senkyoku/CCAminToSenkyokuKokai/
+
+
