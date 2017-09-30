2ee05be6d08ad1d9834a041a19fc722696c537bc
tetsuyas <hoge@aaa.cccc>
2017-09-30 19:50:41 +0900

    保持しているcellsから、指定された町字を持つcellsを返却

--- script/city.js
+++ script/city.js
@@ -27,6 +27,22 @@ module.exports = class City{
   isDevided(){
     return this.senkyokuNums.length > 1;
   }
+
+  /**
+   * 与えら得た町字名と一致する又はそれを含むcellを返す
+   * @returns {string}
+   */
+  searchCellsIncludingTownName(townName){
+    const same = [];
+    const includes = [];
+    for(let i = 0; i < this.cells.length; i = i + 1 ){
+      const cell =  this.cells[i];
+      if(cell.townName()===townName) same.push(cell);
+      if(cell.townName().includes(townName)) includes.push(cell);
+    }
+    if(same.length > 0) return same;
+    return includes;
+  }
   message(){
     if(this.isDevided()){
       return `${this.cityName}(${this.cityCode})は複数選挙区${this.senkyokuNums.join("区,") + "区"}に分割されてます`;
