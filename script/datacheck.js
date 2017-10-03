2205c2666f323e8a066b9a36ef7118e03549124f
Tetsuya <tetsuyas1@gmail.com>
2017-10-03 12:53:41 +0900

    テストのスクリプトを作成

--- script/datacheck.js
+++ script/datacheck.js
@@ -2,10 +2,31 @@
 
 const fs = require("fs-extra");
 
-const src = `${__dirname}/../data/json/postal2senkyoku.json`;
+const srcDefault = `${__dirname}/../data/json/postal2senkyoku.json`;
 const srcLight = `${__dirname}/../data/json/postal2senkyoku.light.json`;
 
-const 
+const PostalCodeReader = require("./postal_code_reader");
 
-const srcObj = JSON.parse(fs.readFileSync(src).toString());
+const pcr = new PostalCodeReader();
+let errors = [];
 
+let srcObj = JSON.parse(fs.readFileSync(srcDefault).toString());
+Object.keys(pcr.postalCodes).forEach((k)=>{
+  const res = srcObj[k];
+  if(!res) errors.push(k);
+});
+
+console.log(`標準サイズの　${Object.keys(srcObj).length}の要素のある${srcDefault} をチェックします。結果${errors.length}のエラー`);
+
+errors = [];
+srcObj = JSON.parse(fs.readFileSync(srcLight).toString());
+Object.keys(pcr.postalCodes).forEach((k)=>{
+  let res = null;
+  res = srcObj[k.substring(0,3)]
+  if(!res){
+    const res = srcObj[k];
+    if(!res) errors.push(k);
+  }
+});
+
+console.log(`標準サイズの　${Object.keys(srcObj).length}の要素のある${srcLight} をチェックします。結果${errors.length}のエラー`);
