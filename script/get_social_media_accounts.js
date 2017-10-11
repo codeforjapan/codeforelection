dc8e0ab6f41f9c6ca7b9932a3e2b5f156557679b
Tetsuya <tetsuyas1@gmail.com>
2017-10-12 01:46:19 +0900

    FB/TW Accounts by Bing Search API

--- script/get_social_media_accounts.js
+++ script/get_social_media_accounts.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const https = require('https');
 const fs = require("fs-extra");
 const csvSync = require('csv-parse/lib/sync');
-
+const url = require('url');
 /*
  https://api.cognitive.microsoft.com/bing/v7.0/images/search[?q][&count][&offset][&mkt][&safeSearch]
  console.log(process.env.AZULE_BING_SEARCH_API_KEY_1);
@@ -31,17 +31,23 @@ class SearchResult{
     this.twitter = null;
     this.facebook = null;
     if(this.url.includes("https://twitter.com/")){
-      this.twitter = this.url.replace("https://twitter.com/","")
+      const myURL = url.parse(this.url);
+      this.twitter = myURL.pathname;
+      if(this.twitter){
+        this.twitter = this.twitter.slice(1);
+      }
     }
     if(this.url.includes("https://facebook.com/")){
-      this.facebook = this.url.replace("https://facebook.com/","")
+      const myURL = url.parse(this.url);
+      if(this.facebook){
+        this.facebook = this.facebook.slice(1);
+      }
     }
   }
 }
 
-function search(obj, site="twitter.com", size=3){
+function search(obj, site="facebook.com", size=3){
   const query = `${obj["sei"]} ${obj["mei"]} ${obj["seikana"]} ${obj["meikana"]} site:${site}`;
-
   console.log(query);
   const encode_query = encodeURIComponent(query);
   const options = {
@@ -54,51 +60,105 @@ function search(obj, site="twitter.com", size=3){
       'Ocp-Apim-Subscription-Key': process.env.AZULE_BING_SEARCH_API_KEY_1
     }
   };
-  console.log(options);
   return new Promise((r,j)=>{
     const res = [];
     const request = https.request(options, (response) => {
+      //console.log('statusCode:', response.statusCode);
+      //console.log('headers:', response.headers);
       response.setEncoding('utf8');
       response.on('data', (data) => {
-        const values = data.webPages.value;
-        console.log(values);
-        for(let i = 0 ;i < values.length; i = i + 1){
-          res.push(new SearchResult(values[i]));
+        try{
+          //console.log("data="+ data.toString());
+          const values = JSON.parse(data).webPages.value;
+          //console.log(values);
+          for(let i = 0 ;i < values.length; i = i + 1){
+            res.push(new SearchResult(values[i]));
+          }
+        }catch(e){
+          r(res);
         }
-        r(res);
       });
-      response.on('end', () => { console.log("end"); r(res); });
+      response.on('end', () => { r(res); });
     });
     request.on('error', (e) => { j(e); });
+    request.end();
   });
 }
 
 
 const hmr = new HaakuMoreReader();
-const array = hmr.lines.slice(0,3);
+const array = hmr.lines;
 const results = {};
-
+const lines = [];
+const size = 3;
 let count = 0;
-let prom = search((array[count]));
+
+console.log(JSON.stringify(array[count]));
+let tw = search(array[count], "twitter.com", size);
+let fb = search(array[count], "www.facebook.com", size);
+
 let to = setInterval(function(){
-  console.log(`c = ${count}`);
-  console.log(`prom = ${prom.toString()}`);
-  prom.then((res)=>{
-    console.log(`res = ${res}`);
-    results[array[count].kanaid] = res;
+  let result = [];
+  tw.then((res)=> {
+    console.log("====res");
+    console.log(res);
+    for(let i = 0 ; i < size; i = i + 1) {
+      if (res[i]) {
+        const obj = res[i];
+        result.push(obj.url);
+        result.push(obj.name);
+        result.push(obj.snippet);
+        result.push(obj.twitter);
+      } else {
+        for (let j = 0; j < 4; j = j + 1) {
+          result.push("");
+        }
+      }
+    }
+    if(count < array.length - 1){
+      console.log(JSON.stringify(array[count+1]));
+      tw = search(array[count+1], "twitter.com", size);
+    }
+    return fb;
+  }).then((res)=>{
+    console.log("====res");
+    console.log(res);
+    for(let i = 0 ; i < size; i = i + 1){
+      if(res[i]){
+        const obj = res[i];
+        result.push(obj.url);
+        result.push(obj.name);
+        result.push(obj.snippet);
+        result.push(obj.facebook);
+      }else{
+        for(let j = 0 ; j < 4; j = j + 1){
+          result.push("");
+        }
+      }
+    }
+    console.log("result=====");
+    console.log(result);
+    results[array[count].kanaid] = result;
+    lines.push(`${array[count].kanaid},${result.join(",")}`);
+
+    if(count < array.length - 1 ){
+      fb = search(array[count+1], "www.facebook.com", size);
+    }
+
     count = count + 1;
-    prom = search(array[count]);
-    if(count === array.length){
+    if(count == array.length){
       clearInterval(to);
-      console.log(results);
-      return false;
+      fs.writeFileSync(`${__dirname}/../data/graydb/haaku_sns.json`, JSON.stringify(results));
+      fs.writeFileSync(`${__dirname}/../data/graydb/haaku_sns.csv`, lines.join("\n"));
+      const execSync = require('child_process').execSync;
+      execSync(`nkf -s ${__dirname}/../data/graydb/haaku_sns.csv > ${__dirname}/../data/graydb/haaku_sns.sjis.csv`);
     }
     return false;
   }).catch((e)=>{
-    count = count + 1
+    count = count + 1;
     console.log(e);
   })
-},3000);
+},1000);
 
 
 
