245de660ad94b1f32990808b8d10765a6afb7e2b
Tetsuya <tetsuyas1@gmail.com>
2017-10-12 10:38:17 +0900

    Refectoring SNS accounts.

--- script/get_social_media_accounts.js
+++ script/get_social_media_accounts.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const https = require('https');
 const fs = require("fs-extra");
 const csvSync = require('csv-parse/lib/sync');
-const url = require('url');
+const url_lib = require('url');
 /*
  https://api.cognitive.microsoft.com/bing/v7.0/images/search[?q][&count][&offset][&mkt][&safeSearch]
  console.log(process.env.AZULE_BING_SEARCH_API_KEY_1);
@@ -23,32 +23,22 @@ class HaakuMoreReader{
   }
 }
 
-class SearchResult{
-  constructor(obj){
-    this.url = obj.url;
-    this.name = obj.name;
-    this.snippet = obj.snippet;
-    this.twitter = null;
-    this.facebook = null;
-    if(this.url.includes("https://twitter.com/")){
-      const myURL = url.parse(this.url);
-      this.twitter = myURL.pathname;
-      if(this.twitter){
-        this.twitter = this.twitter.slice(1);
-      }
-    }
-    if(this.url.includes("https://facebook.com/")){
-      const myURL = url.parse(this.url);
-      if(this.facebook){
-        this.facebook = this.facebook.slice(1);
-      }
-    }
+function snsId(url){
+  if(!url.includes("https://twitter.com/") && !url.includes("https://www.facebook.com/")) return "";
+  const p = url_lib.parse(url).pathname;
+  if(!p) return "" ;
+  if(p.startsWith("/")){
+    return p.slice(1);
+  }else{
+    return p
   }
 }
 
-function search(obj, site="facebook.com", size=3){
-  const query = `${obj["sei"]} ${obj["mei"]} ${obj["seikana"]} ${obj["meikana"]} site:${site}`;
-  console.log(query);
+function query(obj, site){
+  return `${obj["sei"]} ${obj["mei"]} ${obj["seikana"]} ${obj["meikana"]} site:${site}`;
+}
+
+function search(query, kanaid, size=3){
   const encode_query = encodeURIComponent(query);
   const options = {
     hostname: 'api.cognitive.microsoft.com',
@@ -61,22 +51,15 @@ function search(obj, site="facebook.com", size=3){
     }
   };
   return new Promise((r,j)=>{
-    const res = [];
+    let res = null;
     const request = https.request(options, (response) => {
       //console.log('statusCode:', response.statusCode);
       //console.log('headers:', response.headers);
       response.setEncoding('utf8');
       response.on('data', (data) => {
-        try{
-          //console.log("data="+ data.toString());
-          const values = JSON.parse(data).webPages.value;
-          //console.log(values);
-          for(let i = 0 ;i < values.length; i = i + 1){
-            res.push(new SearchResult(values[i]));
-          }
-        }catch(e){
-          r(res);
-        }
+        res = JSON.parse(data);
+        res.query = query;
+        res.kanaid = kanaid;
       });
       response.on('end', () => { r(res); });
     });
@@ -88,70 +71,43 @@ function search(obj, site="facebook.com", size=3){
 
 const hmr = new HaakuMoreReader();
 const array = hmr.lines;
-const results = {};
 const lines = [];
 const size = 3;
 let count = 0;
 
 console.log(JSON.stringify(array[count]));
-let tw = search(array[count], "twitter.com", size);
-let fb = search(array[count], "www.facebook.com", size);
+let tw = search(query(array[count], "twitter.com"), array[count].kanaid, size);
+let fb = search(query(array[count], "www.facebook.com"), array[count].kanaid, size);
 
 let to = setInterval(function(){
-  let result = [];
   tw.then((res)=> {
-    console.log("====res");
-    console.log(res);
-    for(let i = 0 ; i < size; i = i + 1) {
-      if (res[i]) {
-        const obj = res[i];
-        result.push(obj.url);
-        result.push(obj.name);
-        result.push(obj.snippet);
-        result.push(obj.twitter);
-      } else {
-        for (let j = 0; j < 4; j = j + 1) {
-          result.push("");
+    if(res && res.webPages && res.webPages.value){
+      const v = res.webPages.value;
+      for(let i = 0 ; i < v.length; i = i + 1) {
+        if (v[i]) {
+          lines.push([res.kanaid, 'tw', res.query, res.queryContext.originalQuery, snsId(v[i].url), v[i].url, v[i].name, v[i].snippet].join(","));
         }
       }
     }
-    if(count < array.length - 1){
-      console.log(JSON.stringify(array[count+1]));
-      tw = search(array[count+1], "twitter.com", size);
-    }
+    if(count < array.length - 1) tw = search(query(array[count], "twitter.com"), array[count].kanaid, size);
     return fb;
   }).then((res)=>{
-    console.log("====res");
-    console.log(res);
-    for(let i = 0 ; i < size; i = i + 1){
-      if(res[i]){
-        const obj = res[i];
-        result.push(obj.url);
-        result.push(obj.name);
-        result.push(obj.snippet);
-        result.push(obj.facebook);
-      }else{
-        for(let j = 0 ; j < 4; j = j + 1){
-          result.push("");
+    if(res && res.webPages && res.webPages.value) {
+      const v = res.webPages.value;
+      for (let i = 0; i < v.length; i = i + 1) {
+        if (v[i]) {
+          lines.push([res.kanaid, 'fb', res.query, res.queryContext.originalQuery, snsId(v[i].url), v[i].url, v[i].name, v[i].snippet].join(","));
         }
       }
     }
-    console.log("result=====");
-    console.log(result);
-    results[array[count].kanaid] = result;
-    lines.push(`${array[count].kanaid},${result.join(",")}`);
-
-    if(count < array.length - 1 ){
-      fb = search(array[count+1], "www.facebook.com", size);
-    }
-
+    if(count < array.length - 1 ) fb = search(query(array[count], "www.facebook.com"), array[count].kanaid, size);
     count = count + 1;
     if(count == array.length){
+      console.log(lines);
       clearInterval(to);
-      fs.writeFileSync(`${__dirname}/../data/graydb/haaku_sns.json`, JSON.stringify(results));
-      fs.writeFileSync(`${__dirname}/../data/graydb/haaku_sns.csv`, lines.join("\n"));
+      fs.writeFileSync(`${__dirname}/../data/graydb/haaku_sns_results.csv`, lines.join("\n"));
       const execSync = require('child_process').execSync;
-      execSync(`nkf -s ${__dirname}/../data/graydb/haaku_sns.csv > ${__dirname}/../data/graydb/haaku_sns.sjis.csv`);
+      execSync(`nkf -s ${__dirname}/../data/graydb/haaku_sns_results.csv > ${__dirname}/../data/graydb/haaku_sns_results.sjis.csv`);
     }
     return false;
   }).catch((e)=>{
